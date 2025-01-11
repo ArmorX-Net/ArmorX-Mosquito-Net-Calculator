bdc166f12e50a1f6e2a4f6e483f435c923e673f2
ArmorX-Net <mbventures.amazon@gmail.com>
2025-01-11 21:40:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -487,7 +487,7 @@ function formatMessageForWhatsApp() {
             if (lines.some(line => line.includes('Closest Match Found'))) {
                 const customSizeDetail = lines.find(line => line.startsWith('- Custom Size Needed'));
                 const customSizeInCm = lines.find(line => line.startsWith('- Custom Size in Cm')); // New detail for Custom Size in Cm
-                const closestSizeDetail = lines.find(line => line.startsWith('- Closest Size Ordered'));
+                const closestSizeDetail = lines.find(line => line.startsWith('- Closest Size to Ordered'));
                 const colorDetail = lines.find(line => line.startsWith('- Color'));
                 const linkDetail = lines.find(line => line.startsWith('- Link'));
 
